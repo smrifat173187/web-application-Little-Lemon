ca7e74c3189102d240bf49def2e6f2efc9848445
smrifat173187 <142487757+smrifat173187@users.noreply.github.com>
2024-06-05 09:00:52 +0600

    Update MySQL database.py

---
+++ MySQL database.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
